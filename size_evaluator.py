1a5bd73d98f3a75379d798fe806684740e8922c6
Willy <b04502139@ntu.edu.tw>
2019-06-13 15:53:59 +0800

    #0 circuit size evaluator

---
+++ size_evaluator.py
@@ -0,0 +1,11 @@
+import sys
+
+size = 0
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        line = line.strip().split()
+        if len(line) == 0: continue
+        g_type = line[0]
+        if g_type == 'AND2' or g_type == 'OR2' or g_type == 'XOR2' or g_type == 'NOR2' or g_type == 'XNOR2' or g_type == 'NAND2': size += 2
+
+print ('circuit size:', size)
